6cc7e58e051a178f7197da7f02ba2b255f270cfc
gaoyixu <985627758@qq.com>
2018-12-01 15:38:49 +0800

    Update model

--- model.py
+++ model.py
@@ -82,9 +82,8 @@ class Model:
             y_hat = tf.argmax(predicts, axis=-1)
         with tf.name_scope('loss'):
             if self.loss_mode == 'MSE':
-                loss = tf.reduce_sum(
-                    (tf.reshape(output_y, [-1]) - tf.reshape(y_hat, [-1]))
-                    / tf.to_float(self.batch_size))
+                loss = tf.reduce_mean(
+                    tf.square(tf.reshape(output_y, [-1]) - tf.reshape(y_hat, [-1])))
             elif self.loss_mode == 'CE':
                 cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
                     logits=logits, labels=tf.reshape(output_y, [-1]))
